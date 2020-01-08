1274658b7317b1f74e46f9c77b47db9c661482b6
jerk01 <awesome.s4m@yandex.ru>
2020-01-08 16:50:49 +1000

    drom and web added

---
+++ python/drom/drom_poll_q1_forapload.py
@@ -0,0 +1,68 @@
+import pandas as pd
+import numpy as np
+import gc
+
+ru_patterns = " .ru;. ru; .ru;,ru ;, ru ; ,ru ;,.ru;, .ru; ru "
+dromru_patterns = "drom ru;drom,ru;дром.ру;д-ром;дром.ru;дромру;dromru"
+autoru_patterns = "auto ru;auto,ru;авто.ру;авто.ru;автору"
+avitoru_patterns = "avito ru;avito,ru;авито.ру;авито.ru;авитору"
+e1ru_patterns = "e1 ru;e1,ru;е1.ру;e1.ru;e1ru"
+yoularu_patterns = "youla ru;youla,ru;youla;юла.ру;юла.ru;юлару"
+# value '1' assume to be e1
+
+domains = ["drom.ru", "auto.ru", "avito.ru", "e1.ru", "youla.ru"]
+# patterns и domains строго в соответствующем порядке !
+and_patterns = [';', " и "]
+
+def replace_patterns():
+
+    rupatterns = ru_patterns.split(';')
+    patternlist = [
+        dromru_patterns.split(";"),
+        dromru_patterns.split(";"),
+        autoru_patterns.split(";"),
+        avitoru_patterns.split(";"),
+        e1ru_patterns.split(";"),
+        yoularu_patterns.split(";")
+    ]
+
+    dataset
+    dataset['q1'] = dataset['q1'].str.lower()
+    dataset['q1'] = dataset['q1'].str.replace("'", "")
+
+    for andp in and_patterns:
+        dataset['q1'] = dataset['q1'].str.replace(andp, ',')
+    
+    for ru in rupatterns:
+        dataset['q1'] = dataset['q1'].str.replace(ru, ".ru")
+
+    for dmn in domains:
+        for patterntype in patternlist:
+            for pattern in patterntype:
+                dataset['q1'] = dataset['q1'].str.replace(pattern, dmn)
+
+replace_patterns()
+
+q1sr = dataset['q1'].str.split(",", expand = True)
+print(q1sr.head(10))
+
+colnames = ["q1.{}".format(col+1) for col in q1sr]
+
+q1sr = q1sr.set_axis(colnames, axis=1, inplace=False)
+dataset = dataset.join(q1sr)
+del q1sr
+dataset.drop(columns=["q1"], inplace=True)
+print(dataset.head(10))
+dataset = pd.melt(dataset, id_vars="id", var_name="Attribute", value_name="a1")
+print(dataset.head(10))
+dataset.drop(columns=["Attribute"], inplace=True)
+print(dataset.head(10))
+
+dataset['a1'] = dataset['a1'].replace("", np.nan, inplace=True)
+print(dataset.head(10))
+dataset['a1'] = dataset['a1'].dropna()
+print(dataset.head(10))
+dataset['a1'] = dataset['a1'].str.lstrip()
+print(dataset.head(10))
+
+gc.collect()
