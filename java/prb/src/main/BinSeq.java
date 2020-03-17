5bcf063b452ab16ff5623de5326aac8241d90103
jerk01 <awesome.s4m@yandex.ru>
2020-03-17 12:26:27 +1000

    bigdata pandas-lab done

--- java/prb/src/main/BinSeq.java
+++ java/prb/src/main/BinSeq.java
@@ -1,6 +1,8 @@
 package main;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 import static java.lang.Math.pow;
@@ -9,14 +11,14 @@ public class BinSeq {
 
     int N;
 
-    public void preOrderWrite1(BinTree.Node node, FileWriter fw, int pos) throws IOException {
+    private static void preOrderWrite1(BinTree.Node node, FileWriter fw) throws IOException {
         if (node == null) return;
+//        System.out.print(node.val);
         fw.write(
                 node.val ? '1' : '0'
         );
-        if (pos == N) fw.write('\n');
-        preOrderWrite1(node.left, fw, pos+1);
-        preOrderWrite1(node.right, fw, pos+1);
+        preOrderWrite1(node.left, fw);
+        preOrderWrite1(node.right, fw);
     }
 
     public void preOrderWrite0(BinTree.Node node, FileWriter fw) {
@@ -33,14 +35,15 @@ public class BinSeq {
         sc.close();
 
         BinTree bt1 = new BinTree().newTree(N, true);
-        BinTree bt0 = new BinTree().newTree(N, false);
+//        BinTree bt0 = new BinTree().newTree(N, false);
 
         BinTree.Node node;
         node = bt1.getRoot();
 
-
         File output = new File("output.txt");
         FileWriter fw = new FileWriter(output);
+
+        preOrderWrite1(node, fw);
 
         fw.close();
     }
@@ -52,18 +55,20 @@ public class BinSeq {
         public BinTree newTree(int N, boolean rootval) {
 
             BinTree binTree = new BinTree();
-
-            for (int i = 0; i <= N; i++) {
-                binTree.add(rootval);
-                binTree.add(!rootval);
+            binTree.add(rootval);
+            for (int i = 1; i < N; i++) {
+                binTree.add(true);
+                binTree.add(false);
             }
+
             return binTree;
         }
 
-        public void add (boolean val) {
+        public void add(boolean val){
 
             root = addReq(root, val);
         }
+
 
         public Node getRoot() {
             return root;
@@ -71,13 +76,20 @@ public class BinSeq {
 
         private Node addReq(Node curr, boolean val) {
 
-            if (curr == null)
+            if (curr == null){
+                System.out.println("new " + val);
                 return new Node(val);
+            }
 
             if (val) {
-                return addReq(curr.left, val);
-            } else
-                return addReq(curr.right, val);
+                System.out.println("addreq left");
+                curr.left = addReq(curr.left, true);
+                return curr.left;
+            } else {
+                System.out.println("addreq right");
+                curr.right = addReq(curr.right, false);
+                return curr.right;
+            }
 
         }
 
--- java/prb/src/main/P6.java
+++ java/prb/src/main/P6.java
@@ -6,21 +6,11 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-import static java.lang.Math.pow;
 
 public class P6 {
 
     static int N, counter;
 
-
-    public static void binSeq(FileWriter fw, int counter){
-
-        if (counter == pow(2,N))
-            return;
-
-
-        binSeq(fw, counter+1);
-    }
 
     public static void main(String[] args) throws IOException {
 
@@ -36,7 +26,7 @@ public class P6 {
         File output = new File("output.txt");
         FileWriter fw = new FileWriter(output);
 
-        binSeq(fw, 1);
+
 
         fw.close();
     }
